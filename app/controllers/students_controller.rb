f332ec2d1f79c3df785c2cd6bd7b2f530c6375ad
Justin Daniel <justinddaniel@gmail.com>
2017-11-09 01:18:41 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,32 @@
 class StudentsController < ApplicationController
+  def index
+		@schoolclasses = SchoolClass.all
+	end
+
+	def show
+		@schoolclass = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@schoolclass = SchoolClass.new
+	end
+
+	def create
+	  @schoolclass = SchoolClass.new
+	  @schoolclass.title = params[:title]
+	  @schoolclass.room_number = params[:room_number]
+	  @schoolclass.save
+	  redirect_to school_class_path(@schoolclass)
+	end
+
+	def edit
+		@schoolclass = SchoolClass.find(params[:id])
+	end
+
+	def update
+	  @schoolclass = SchoolClass.find(params[:id])
+	  @schoolclass.update(params.require(:schoolclass))
+	  redirect_to school_class_path(@schoolclass)
+	end
 end
+
